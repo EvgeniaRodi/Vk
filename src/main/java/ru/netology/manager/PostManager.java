a898afd8713c52101e8f71c6da1945cf9aa2f871
Rodionova Evgenia <jenechkajj353@gmail.com>
2021-05-17 10:03:17 +0300

    manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,29 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Geo;
+import ru.netology.domain.Post;
+
+public class PostManager {
+
+    private Post[] posts;
+
+
+    public Post[] search (int ownerId, String domain, boolean ownersOnly, int offset, int friendsOnly) {
+
+        //  create some logic
+
+        return null;
+    }
+
+        public Geo [] geos;
+
+    public Geo [] search (String type, String coordinates){
+
+        // create some logic
+
+     return null;
+
+    }
+
+
+}
