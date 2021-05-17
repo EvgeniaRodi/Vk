45966cfd6e0651296c0c1ef39a5d68f0ad0d3ca5
Rodionova Evgenia <jenechkajj353@gmail.com>
2021-05-17 09:31:11 +0300

    createDeveloperInfo

---
+++ src/main/java/ru/netology/domain/Placeholder.java
@@ -0,0 +1,8 @@
+
+package ru.netology.domain;
+
+public class Placeholder {
+    private String placeholder; {
+        System.out.println ("без подписки");
+    }
+}
--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -14,10 +14,35 @@ public class Post {
     private boolean textPost;
     private boolean mixedPost;
 
+    private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private String postType;
+    private int signerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private int postponedId;
+
     private CommentsInfo commentsInfo;
     private LikesInfo likesInfo;
     private RepostsInfo repostsInfo;
     private ViewsInfo viewsInfo;
+    private Copyright copyright;
+    private Donut donut;
+    private Geo geo;
+    private PostSource postSource;
+    private Object object;
+
+
+
+
 
     // + get/set на все поля
 
